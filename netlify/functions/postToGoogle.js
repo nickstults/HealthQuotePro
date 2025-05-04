d4f9e93720f1b642a142a4e1bdc04583b4893538
nickstults <nicholas.stults@gmail.com>
2025-05-04 16:45:56 -0400

    Update postToGoogle.js

--- netlify/functions/postToGoogle.js
+++ netlify/functions/postToGoogle.js
@@ -3,7 +3,7 @@ const fetch = require("node-fetch");
 exports.handler = async function (event) {
   const data = JSON.parse(event.body);
 
-  const response = await fetch("https://script.google.com/macros/s/PASTE_YOUR_SCRIPT_URL_HERE/exec", {
+  const response = await fetch("https://script.google.com/macros/s/AKfycbxrd1QfKygzfiDKCfwADjer_kQYAP_DbPe1w4L-f3_dF_uNldZJ6Bpv4r5SEsD782ZW/exec", {
     method: "POST",
     headers: { "Content-Type": "application/x-www-form-urlencoded" },
     body: new URLSearchParams(data).toString()
