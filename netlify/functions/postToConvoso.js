a686071984ce8678186163b8cf7fd309742a26ae
nickstults <nicholas.stults@gmail.com>
2025-05-04 16:48:28 -0400

    Update postToConvoso.js

--- netlify/functions/postToConvoso.js
+++ netlify/functions/postToConvoso.js
@@ -3,31 +3,40 @@ const fetch = require("node-fetch");
 exports.handler = async function (event) {
   const data = JSON.parse(event.body);
 
-  const convosoUrl = "https://api.convoso.com/v1/leads/insert";
-  const token = "YOUR_CONVOSO_API_TOKEN";
-
-  const payload = {
-    auth_token: token,
-    list_id: 9689,
-    phone_number: data.phone_number,
-    first_name: data.first_name,
-    last_name: data.last_name,
-    address1: data.address,
-    email: data.email,
-    date_of_birth: data.date_of_birth,
-    ip_address: data.ip_address,
+  const params = new URLSearchParams({
+    auth_token: "sg19yks0iek24aeebmmgebhsuwxsmpd4",
+    list_id: "9689",
+    phone_code: "1",
+    check_dup: "0",
+    check_dup_archive: "0",
+    check_dnc: "0",
+    check_wireless: "0",
+    hopper: "1",
+    hopper_priority: "99",
+    hopper_expires_in: "0",
+    update_order_by_last_called_time: "DESC",
+    lead_id: "0",
     publisher: "healthquotepros",
-  };
+    first_name: data.first_name || "",
+    last_name: data.last_name || "",
+    phone_number: data.phone_number || "",
+    email: data.email || "",
+    address1: data.address || "",
+    date_of_birth: data.date_of_birth || "",
+    ip_address: data.ip_address || "",
+  });
 
-  const response = await fetch(convosoUrl, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(payload),
+  const url = `https://api.convoso.com/v1/leads/insert?${params.toString()}`;
+
+  const response = await fetch(url, {
+    method: "GET"
   });
 
   const result = await response.json();
+
   return {
     statusCode: 200,
     body: JSON.stringify(result),
   };
 };
+
