63f61de7b220f0d572e5efef062a4d1de9f4d3d0
nickstults <nicholas.stults@gmail.com>
2025-05-04 16:42:51 -0400

    Update postToGoogle.js

--- netlify/functions/postToGoogle.js
+++ netlify/functions/postToGoogle.js
@@ -1,21 +1,17 @@
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 
-exports.handler = async (event) => {
+exports.handler = async function (event) {
   const data = JSON.parse(event.body);
-  const form = new URLSearchParams();
 
-  for (let key in data) {
-    form.append(key, data[key]);
-  }
-
-  const response = await fetch("https://script.google.com/macros/s/AKfycbyep7oK-fRP8Ha4_Hu0aMM_t-Cz5ozuqic939Xtj33bn6hmwE9zqQZZNAZP2tsVUbFx/exec", {
+  const response = await fetch("https://script.google.com/macros/s/PASTE_YOUR_SCRIPT_URL_HERE/exec", {
     method: "POST",
-    body: form
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams(data).toString()
   });
 
-  const resText = await response.text();
+  const result = await response.json();
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: 'Posted to Google Sheets', googleResponse: resText })
+    body: JSON.stringify(result),
   };
 };
